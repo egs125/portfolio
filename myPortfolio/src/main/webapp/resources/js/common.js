376ab90bd921c8dcc68b4dbea5344274a96b08f6
KOEUN SEO <egs125@gmail.com>
2019-02-19 14:16:49 +0900

    Update common.js
    change nav function

--- myPortfolio/src/main/webapp/resources/js/common.js
+++ myPortfolio/src/main/webapp/resources/js/common.js
@@ -1,15 +1,10 @@
-//OPEN 클릭 시 사이드메뉴 펼침
 $(document).ready(function(){
 	
-	$("#topBar span").on("click", function(){
-		$("#sideNav").attr("style", "width:250px");
-		$("#main").attr("style", "margin-left:250px");
-	});
-
-	//사이드메뉴의 X 버튼 클릭 시 사이드메뉴 접음
-	$("#sideNav span").on("click", function(){
-		$("#sideNav").attr("style", "width:0");
-		$("#main").attr("style", "margin-left:0");
-	});
+	var nav = $(".jumbotron h2").html();
+	
+	if(nav){
+		$(".navbar-nav li").removeClass("active");
+		$("#nav_" + nav.toLowerCase()).addClass("active");
+	}
 	
 });
